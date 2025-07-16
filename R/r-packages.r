115653bda35f670c0dc98f94e94381f96f3abc29
Adam Blake <adam@ablake.dev>
2025-07-16 10:21:00 -0700

    feat(r): add tools for R package development

--- R/r-packages.r
+++ R/r-packages.r
@@ -1,17 +1,27 @@
-options(repos = c(CRAN = "https://packagemanager.posit.co/cran/latest"))
+options(repos = c(
+  CRAN = "https://packagemanager.posit.co/cran/latest",
+  "https://community.r-multiverse.org"
+))
 
 if (!requireNamespace("pak", quietly = TRUE)) {
   install.packages("pak")
 }
 
 pak::pkg_install(upgrade = FALSE, c(
+  # common tools
   "jsonlite",
   "remotes",
   "rlang",
   "tidyverse",
+  "coursekata",
+  # package development
   "devtools",
+  "rhub",
   "roxygen2",
+  "spelling",
   "testthat",
   "usethis",
-  "languageserver"
+  # vscode
+  "languageserver",
+  "httpgd"
 ))
